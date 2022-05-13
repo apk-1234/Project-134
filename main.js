6a64ba4f5f14a741c4301c901362cdc836cc92d2
apk-1234 <81460947+apk-1234@users.noreply.github.com>
2022-05-13 21:13:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+img = "";
+objects = [];
+status = "";
+alarm="";
+function preload(){
+  alarm = loadSound('danger-alarm-23793.mp3');
+}
+
+function setup() {
+  canvas = createCanvas(380, 380);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(380,380);
+  video.hide();
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML = "Status : ";
+}
+
+function modelLoaded() {
+  console.log("Model Loaded!")
+  status = true;
+}
+
+function gotResult(error, results) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(results);
+  objects = results;
+}
+
+function draw() {
+  image(video, 0, 0, 380, 380);
+      if(status != "")
+      {
+        r =  random(255);
+        g =  random(255);
+        b =  random(255);      
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++) {
+          if(objects[i].label=="person")
+          {
+            document.getElementById("status").innerHTML = "Status : Person found";
+          }
+          else
+          {
+            document.getElementById("status").innerHTML = "Status : Person not found";
+          }
+          if(document.getElementById("status").innerHTML == "Status : Person found")
+          {
+            alarm.stop();
+          } 
+          else
+          {
+            alarm.play();
+          }
+          fill(r,g,b);
+          percent = floor(objects[i].confidence * 100);
+          text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+          noFill();
+          stroke(r,g,b);
+          rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+      }
+}
